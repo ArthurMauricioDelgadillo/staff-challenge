fb01703e1cb904d3313065e79a30f5568184475b
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2018-02-27 18:11:54 -0500

    feat: add scroll to top

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {
     BrowserRouter as Router,
     Route,
-    Redirect
+    Redirect,
+    withRouter
 } from 'react-router-dom'
 
 import Checkout from './pages/checkout'
 
+
+const ScrollToTop = withRouter(class extends Component {
+    componentDidUpdate(prevProps) {
+      if (this.props.location !== prevProps.location) {
+        window.scrollTo(0, 0)
+      }
+    }
+  
+    render() {
+      return this.props.children
+    }
+  })
+
 export default () =>
     <Router>
-        <div>
+        <ScrollToTop>
             <Route exact path="/" 
                 render={() => <Redirect to="/checkout" />} />
             <Route path="/checkout" component={Checkout} />
-        </div>
+        </ScrollToTop>
     </Router>
