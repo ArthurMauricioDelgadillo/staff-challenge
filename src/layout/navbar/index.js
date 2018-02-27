f407900654b24b9899478cf9bd1e9ebc8ffcc938
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2018-02-26 23:21:24 -0500

    feat: add navbar

---
+++ src/layout/navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import logo from '../../logo.svg';
+import './index.css';
+
+export default () => (
+    <Navbar light expand="md">
+        <NavbarBrand href="/"><img src={logo} className="logo" alt="logo" /></NavbarBrand>
+        <Nav className="ml-md-auto d-md-flex pr-4" navbar>
+            <NavItem>
+                <NavLink href="/"><div className="items">6</div> Items</NavLink>
+            </NavItem>
+        </Nav>
+    </Navbar>
+)
