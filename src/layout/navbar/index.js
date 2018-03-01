c91e2f258c5b485ecc504f8bce850373776bd7cd
Arthur Mauricio <amd11dot4@gmail.com>
2018-03-01 22:44:52 +0000

    feat: replace a tags for Link component

--- src/layout/navbar/index.js
+++ src/layout/navbar/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import { Link } from "react-router-dom"
 import logo from '../../logo.svg';
 import './index.css';
 
-export default () => (
+export default ({items}) => (
     <Navbar light expand="md">
-        <NavbarBrand href="/"><img src={logo} className="logo" alt="logo" /></NavbarBrand>
+        <NavbarBrand tag={Link} to="/"><img src={logo} className="logo" alt="logo" /></NavbarBrand>
         <Nav className="ml-md-auto d-md-flex pr-4" navbar>
             <NavItem>
-                <NavLink href="/checkout/step-2"><div className="items">6</div> Items</NavLink>
+                <NavLink tag={Link} to="/checkout/step-2"><div className="items">{items}</div> Items</NavLink>
             </NavItem>
         </Nav>
     </Navbar>
